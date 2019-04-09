e697335697a03b5ecee87f6533f12088716e4109
jasonbowers <jasonbowersaudio@gmail.com>
2019-04-09 01:25:36 -0700

    updating gatsby theme

---
+++ content/meta/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "Jason Bowers", // <title>
+  shortSiteTitle: "Jason Bowers", // <title> ending for posts and pages
+  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteUrl: "",
+  // pathPrefix: "",
+  siteImage: "",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "greg lobinski",
+  authorTwitterAccount: "greglobinski",
+
+  /* info */
+  headerTitle: "Jason Bowers",
+  headerSubTitle: "doing some things with gatsby!",
+
+  /* manifest.json */
+  manifestName: "HeroBlog - a GatsbyJS starter",
+  manifestShortName: "HeroBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/" },
+    { name: "twitter", url: "https://twitter.com/" },
+    { name: "facebook", url: "http://facebook.com/" }
+  ]
+};
